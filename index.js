29d3c2ba8336fdecd8a80c2bbe49699151b50ae7
Farima Torabi <43099035+farimatorabi@users.noreply.github.com>
2021-02-23 17:45:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let emailCollectorForm = document.getElementById("Email-Collector"); 
+
+emailCollectorForm.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    let ourFormData = new FormData(event.target);
+
+    let userFirstName = ourFormData.get("firstName");
+
+    let userEmailAddress = ourFormData.get("emailAddress");
+
+    let ourUpdatedHtmlContent = ` 
+            <h2>Congratulations ${userFirstName}!</h2>
+
+            <p>You're on your way to becoming a BBQ master!</p>
+
+            <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>`
+
+    let ourMainContent = document.getElementById("Main-Content");
+    ourMainContent.innerHTML = ourUpdatedHtmlContent
+});
